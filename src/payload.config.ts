cfd413874807e0764422d3a1f32be2cd786108bb
Nam <namdeveloper.ca@gmail.com>
2023-05-31 15:02:59 +0700

    config docker & env

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,13 +1,14 @@
-import { buildConfig } from 'payload/config';
-import path from 'path';
-import Categories from './collections/Categories';
-import Posts from './collections/Posts';
-import Tags from './collections/Tags';
-import Users from './collections/Users';
-import Media from './collections/Media';
+import { buildConfig } from 'payload/config'
+import path from 'path'
+import Categories from './collections/Categories'
+import Posts from './collections/Posts'
+import Tags from './collections/Tags'
+import Users from './collections/Users'
+import Media from './collections/Media'
 
 export default buildConfig({
-  serverURL: 'http://localhost:3000',
+  // make sure to adjust
+  serverURL: process.env.PAYLOAD_URL,
   admin: {
     user: Users.slug,
   },
